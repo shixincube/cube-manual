f37e0d69df65b2649fd04ce3094fe4b179491dd5
Ambrose Xu <xujiangwei@spap.com>
2021-05-17 09:46:20 +0800

    Update sphinx conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -84,3 +84,9 @@ html_favicon = 'images/favicon.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
